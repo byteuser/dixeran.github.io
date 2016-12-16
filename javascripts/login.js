987c0810088f6d5b574f1ff6b16a6992f59494e9
Dixeran <dixeran@outlook.com>
2016-12-17 00:41:35 +0800

    Add files via upload
    new js

---
+++ javascripts/login.js
@@ -0,0 +1,11 @@
+/**
+ * Created by lenovo on 2016/12/16.
+ */
+
+$(document).ready(function() {
+    $("#sign-in-button").on("click", function () {
+        $("#left-sign-up-container").fadeIn(100);
+        $("#right-sign-up-container").fadeIn(100);
+    });
+
+});
