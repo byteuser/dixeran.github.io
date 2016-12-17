2d0e0deb6a03e15da47609d0e5e7fe1c5dcdbffb
dixeran <dixeran@outlook.com>
2016-12-17 15:10:56 +0800

    Signed-off-by: dixeran <dixeran@outlook.com>

--- javascripts/login.js
+++ javascripts/login.js
@@ -7,5 +7,21 @@ $(document).ready(function() {
         $("#left-sign-up-container").fadeIn(100);
         $("#right-sign-up-container").fadeIn(100);
     });
+    $("#left-sign-up-container").on("mouseover",function () {
+        $("#right-sign-up-container").animate({opacity:"0.3"},150);
+    })
+    $("#right-sign-up-container").on("mouseover",function () {
+        $("#left-sign-up-container").animate({opacity:"0.3"},150);
+    })
+    $("img").on("mouseover",function () {
+        $("#left-sign-up-container").animate({opacity:"1"},150);
+        $("#right-sign-up-container").animate({opacity:"1"},150);
 
+    })
+    $('#sign-in-button').on("mouseover",function () {
+        $("#sign-in-button").animate({opacity:"0.8"},100)
+    })
+    $("#sign-in-button").on("mouseout",function () {
+        $("#sign-in-button").animate({opacity:"1"},150);
+    });
 });
