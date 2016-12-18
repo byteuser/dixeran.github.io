e983eb8256453df908e54748eb2200cae1be497d
dixeran <dixeran@outlook.com>
2016-12-18 23:17:33 +0800

    Signed-off-by: dixeran <dixeran@outlook.com>

--- javascripts/login.js
+++ javascripts/login.js
@@ -8,20 +8,31 @@ $(document).ready(function() {
         $("#right-sign-up-container").fadeIn(100);
     });
     $("#left-sign-up-container").on("mouseover",function () {
-        $("#right-sign-up-container").animate({opacity:"0.3"},150);
-    })
+        $(".padding-right").animate({opacity:"0"},150);
+    });
     $("#right-sign-up-container").on("mouseover",function () {
-        $("#left-sign-up-container").animate({opacity:"0.3"},150);
-    })
+        $(".padding-left").animate({opacity:"0"},150);
+    });
     $("img").on("mouseover",function () {
-        $("#left-sign-up-container").animate({opacity:"1"},150);
-        $("#right-sign-up-container").animate({opacity:"1"},150);
+        $(".padding-left").animate({opacity:"1"},150);
+        $(".padding-right").animate({opacity:"1"},150);
+        $("#sign-in-button").css({"backgroundColor":"steelblue","color":"#ffffff"});
 
-    })
+    });
     $('#sign-in-button').on("mouseover",function () {
-        $("#sign-in-button").animate({opacity:"0.8"},100)
-    })
+        $("#sign-in-button").css({"backgroundColor":"#ffffff","color":"steelblue"});
+    });
     $("#sign-in-button").on("mouseout",function () {
         $("#sign-in-button").animate({opacity:"1"},150);
     });
+    $(".button").on("mouseover",function () {
+        $(".button").css({"backgroundColor":"#ffffff","color":"#ff3c45"});
+
+    });
+    $(".button").on("mouseout",function () {
+        $(".button").css({"backgroundColor":"#ff3c45","color":"#fff"});
+
+    });
+
+
 });
