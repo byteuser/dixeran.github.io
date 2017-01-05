f4ad3904256a4e106348c50f74e9669ebaedd495
dixeran <dixeran@outlook.com>
2017-01-05 19:16:50 +0800

    Signed-off-by: dixeran <dixeran@outlook.com>

--- javascripts/Login-mob.js
+++ javascripts/Login-mob.js
@@ -1,3 +1,20 @@
 /**
  * Created by lenovo on 2017/1/4.
  */
+
+$(document).ready(function () {
+    $('#sign-in-button').on('click',function () {
+        var name = $("#sign-in-name").val();
+        var password = $("#sign-in-password").val();
+        $.post("https://python-dixeran.rhcloud.com/",{method:'in',username:name, password:password},function (data) {
+            alert(data);
+        });
+    });
+    $('#sign-up-button').on('click',function () {
+        var name = $("#sign-up-name").val();
+        var password = $("#sign-up-password").val();
+        $.post("https://python-dixeran.rhcloud.com/",{method:'up',username:name, password:password},function (data) {
+            alert(data);
+        });
+    })
+});
