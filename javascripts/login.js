71bd20d41b487e4c3fba0f6d4d46bb4714ad0b25
dixeran <dixeran@outlook.com>
2017-01-10 16:12:10 +0800

    Signed-off-by: dixeran <dixeran@outlook.com>

--- javascripts/login.js
+++ javascripts/login.js
@@ -3,6 +3,7 @@
  */
 
 $(document).ready(function() {
+
     $("#sign-in-button").on("click", function () {
         $("#left-sign-up-container").fadeIn(100);
         $("#right-sign-up-container").fadeIn(100);
@@ -37,8 +38,7 @@ $(document).ready(function() {
         var name = $("#sign-up-name").val();
         var password = $("#sign-up-password").val();
         $.post("https://python-dixeran.rhcloud.com/",{method:'up',username:name, password:password},function (data) {
-            alert(data);
-            alert('Still working on it...');
+            alert(data+'\n阔以登陆啦');
         });
     });
 
@@ -47,6 +47,15 @@ $(document).ready(function() {
         var password = $("#sign-in-password").val();
         $.post("https://python-dixeran.rhcloud.com/",{method:'in',username:name, password:password},function (data) {
             alert(data);
+            if(data == '登陆成功')
+            {
+                $.post("https://python-dixeran.rhcloud.com/",{method:'code',username:name, password:password},function (decode) {
+                    $.cookie('name',name,{expires:7});
+                    $.cookie('password',password,{expires:7});
+                    $.cookie('code',decode,{expires:7});
+                    window.location.href = 'zone.html'
+                })
+            }
         });
     });
 
