43366c31a74721ae4cde1f389b9ecde4ec37eb57
dixeran <dixeran@outlook.com>
2017-01-14 22:52:52 +0800

    Signed-off-by: dixeran <dixeran@outlook.com>

--- javascripts/zone-mob.js
+++ javascripts/zone-mob.js
@@ -4,6 +4,8 @@
 $(document).ready(function () {
     $('#settings').on('click',function () {
         $('#logout').toggle(300);
-        $('#add').css({'background-color':'#D63932','border-color':'#D63932'});
+        if($('#add').css('background-color') == '#368CBF') {
+            $('#add').css({'background-color': '#D63932', 'border-color': '#D63932'});
+        }
     });
 });
