94eaee06508ed9fc290903b7c5e5ade10802d5b4
dixeran <dixeran@outlook.com>
2017-01-15 21:11:08 +0800

    Signed-off-by: dixeran <dixeran@outlook.com>

--- javascripts/zone-mob.js
+++ javascripts/zone-mob.js
@@ -6,9 +6,15 @@ $(document).ready(function () {
         $('#logout').toggle(300);
         if($('#add').css('background-color') == 'rgb(66, 139, 202)') {
             $('#add').css({'background-color': '#D63932', 'border-color': '#D63932'});
+            $('#add-img').animate({maxWidth:145},{step:function (now) {
+                $('#add-img').css('transform','rotate('+(now-100)+'deg)')
+            },duration:'fast'},'swing')
         }
         else if($('#add').css('background-color') == 'rgb(214, 57, 50)') {
             $('#add').css({'background-color': 'rgb(66, 139, 202)', 'border-color': 'rgb(66, 139, 202)'});
+            $('#add-img').animate({maxWidth:190},{step:function (now) {
+                $('#add-img').css('transform','rotate('+(now-100)+'deg)')
+            },duration:'fast'},'swing')
         }
     });
 });
