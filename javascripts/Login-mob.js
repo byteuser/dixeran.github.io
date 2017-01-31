02d7211378129038eceea9645228f20ad17bb230
dixeran <dixeran@outlook.com>
2017-01-31 20:30:11 +0800

    Signed-off-by: dixeran <dixeran@outlook.com>

--- javascripts/Login-mob.js
+++ javascripts/Login-mob.js
@@ -5,20 +5,25 @@
 $(document).ready(function () {
     var inname = $.cookie('name');
     var inpsd = $.cookie('password');
-    $.post("https://python-dixeran.rhcloud.com/",{ method:'in',username:inname, password:inpsd},function (data) {
-        myApp.alert(data, '登陆消息');
-        if(data == '登陆成功')
-        {
-            mainView.router.loadPage('zone-mob.html');
-        }
-    });
+    if(inname != null) {
+        $.post("https://python-dixeran.rhcloud.com/", {
+            method: 'in',
+            username: inname,
+            password: inpsd
+        }, function (data) {
+            myApp.alert(data, '登陆消息');
+            if (data == '登陆成功') {
+                mainView.router.loadPage('zone-mob.html');
+            }
+        });
+    }
 
 
     $('#sign-in-button').on('click',function () {
         var name = $("#sign-in-name").val();
         var password = $("#sign-in-password").val();
         $.post("https://python-dixeran.rhcloud.com/",{method:'in',username:name, password:password},function (data) {
-            myApp.alert(data);
+            myApp.alert(data,'登录消息');
             if(data == '登陆成功')
             {
                 $.post("https://python-dixeran.rhcloud.com/",{method:'code',username:name, password:password},function (decode) {
--- javascripts/my-app.js
+++ javascripts/my-app.js
@@ -15,7 +15,37 @@ myApp.onPageBeforeInit('zone',function (page) {
         $.cookie('password',null,{expires:7});
         $.cookie('code',null,{expires:7});
         mainView.router.loadPage('index-mob.html');
+    });
+    var myList = myApp.virtualList('.list-block.virtual-list', {
+        // Array with plain HTML items
+        items: [
+            {
+                item:'Item1'
+            }
+        ],
+        renderItem: function (index,item) {
+            return '<li class="item-content">' +
+                '<div class="item-inner">' +
+                '<div class="item-title">'+ index + item.item + '</div>' +
+                '</div>'
+
+        }
+    });
+    $('#add').on('click',function () {
+        myList.appendItem(
+            {
+                item:'test'
+            }
+        );
+    });
+    $('#remove').on('click',function () {
+        var cache = myList.currentToIndex;
+        myList.deleteItem(cache);
     })
+
 });
 
+myApp.onPageAfterAnimation('zone',function (page) {
+
+});
 
