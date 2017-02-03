948ac5eaf72aff196dfd916e6e5b6b9360eb172b
dixeran <dixeran@outlook.com>
2017-02-03 12:50:22 +0800

    Signed-off-by: dixeran <dixeran@outlook.com>

--- javascripts/my-app.js
+++ javascripts/my-app.js
@@ -26,12 +26,17 @@ myApp.onPageBeforeInit('zone',function (page) {
         }
         for(var t = 0;t<list.length;t++)
         {
-            if(list[t][2] == 1)
+            if(list[t][2] == 1 && list[t][5]<list[t][6])
             {
                 var pagenow = list[t][5];
                 var pageall = list[t][6];
                 var progress = (pagenow/pageall)*100;
                 myApp.showProgressbar('#processbar' + t,progress);
+            }
+            else if(list[t][2] == 1 && list[t][5] >= list[t][6])
+            {
+                $('#readprocess' + t).css('display','none');
+                $('#processbar' + t).html("<span class='readprocess'>Finished!   " + '<i class="material-icons">check_box</i></span>');
             }
             else
             {
@@ -71,13 +76,25 @@ myApp.onPageBeforeInit('zone',function (page) {
             return '<li class="accordion-item swipeout">' +
                         '<a href="#" class="item-content item-link">' +
                             '<div class="item-inner">' +
-                                '<div class="item-title"><div class="bookname">' + item.item + '</div>' +
+                                '<div class="item-title list-holder"><div class="bookname">' + item.item + '</div>' +
                                 '<span class="readprocess" id="readprocess' + index + '">&nbsp;Page&nbsp;' + item.pagenow + '&nbsp;of&nbsp;' +item.pageall + '</span>' +
                                 '<p id="processbar'+ index +'"><span></span></p>' +
-                            '</div>' +
+                                '</div>' +
                             '</div>' +
                         '</a>' +
                         '<div class="accordion-item-content">' +
+                                '<div class="list-block">' +
+                                    '<ul><li>' +
+                                        '<div class="item-content">' +
+                                            '<div class="item-inner">' +
+                                                '<div class="item-input input-field" style="width: 70%">' +
+                                                    '<input type="text" placeholder="Today you read.." id="pageaddNum' + index + '">'  +
+                                                '</div>' +
+                                                '<p class="buttons-row"><a href="#" class="button button-fill button-raised pageadd" index="' + index + '">Update</a></p>' +
+                                            '</div>' +
+                                        '</div>' +
+                                    '</li></ul>' +
+                                '</div>' +
                         '<a href="#" class="swipeout-delete button color-red" index="' + index + '">Delete</a>' +
                         '</div>' +
                     '</li>'
@@ -128,18 +145,33 @@ myApp.onPageBeforeInit('zone',function (page) {
             }
         );
     });
-    /* var code = $.cookie('code');
-    $.post("https://python-dixeran.rhcloud.com/fetch",{code:code},function (listData) {
-        var list = $.parseJSON(listData);
-        for(var i = 0;i<list.length;i++)
-        {
-            myList.appendItem(
-                {
-                    item:list[i][3]
-                }
-            )
-        }
-    }); */
+
+    $(document).on('click', '.pageadd',function (e) {
+        var target = e.target;
+        var index = target.getAttribute('index');
+        var pageAdd = parseInt($('#pageaddNum' + index).val());
+        var pageLast = listBody[index][5];
+        pageLast = pageLast + pageAdd;
+        var process = (pageLast / listBody[index][6])*100;
+        myApp.setProgressbar('#processbar' + index,process,1000);
+        myApp.accordionClose('.accordion-item');
+        $('#readprocess' + index).html('&nbsp;Page&nbsp;' + pageLast + '&nbsp;of&nbsp;' + listBody[index][6]);
+        $.post("https://python-dixeran.rhcloud.com/update",{
+            code:$.cookie('code'),
+            bookname: listBody[index][3],
+            bookpagenow: pageAdd
+        },function (data) {
+            myApp.addNotification({
+                message:data,
+                hold:2000
+            });
+            if(process >= 100)
+            {
+                refresh();
+            }
+        });
+    });
+
     $$('.pull-to-refresh-content').on('refresh',function (data) {
         refresh();
         setTimeout(function () {
