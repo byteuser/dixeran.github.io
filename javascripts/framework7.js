dd5173d173bbdf7bbeabbf8f156d2b9eee5bacc7
dixeran <dixeran@outlook.com>
2017-02-03 20:56:00 +0800

    Signed-off-by: dixeran <dixeran@outlook.com>

--- javascripts/framework7.js
+++ javascripts/framework7.js
@@ -7752,7 +7752,7 @@
         ************   App Resize Actions   ************
         ======================================================*/
         // Prevent iPad horizontal body scrolling when soft keyboard is opened
-        /*function _fixIpadBodyScrolLeft() {
+        function _fixIpadBodyScrolLeft() {
             if (app.device.ipad) {
                 document.body.scrollLeft = 0;
                 setTimeout(function () {
@@ -7760,22 +7760,22 @@
                 }, 0);
             }
         }
-        app.initResize = function () {
+         app.initResize = function () {
             $(window).on('resize', app.resize);
             $(window).on('orientationchange', app.orientationchange);
         };
-        app.resize = function () {
+        /*app.resize = function () {
             if (app.sizeNavbars) app.sizeNavbars();
             _fixIpadBodyScrolLeft();
             
-        };
+        };*/
         app.orientationchange = function () {
             if (app.device && app.device.minimalUi) {
                 if (window.orientation === 90 || window.orientation === -90) document.body.scrollTop = 0;
             }
             _fixIpadBodyScrolLeft();
         };
-        */
+
 
         /*===============================================================================
         ************   Store and parse forms data   ************
