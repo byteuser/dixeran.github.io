b292844400cbd89086538a7f5d68b6795518b011
dixeran <dixeran@outlook.com>
2017-05-19 12:10:50 +0800

    Change style in mobile device. Signed-off-by: dixeran <dixeran@outlook.com>

--- DrawNs/rec/script.js
+++ DrawNs/rec/script.js
@@ -203,3 +203,27 @@ var app = new Vue({
         }
     }
 });
+
+$(document).ready(function () {
+    function checkUA() {
+        var userAgentInfo = navigator.userAgent;
+        var Agents = ["Android", "iPhone",
+            "SymbianOS", "Windows Phone",
+            "iPad", "iPod"];
+        var flag = true;
+        for (var v = 0; v < Agents.length; v++) {
+            if (userAgentInfo.indexOf(Agents[v]) > 0) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+    if(!checkUA()){
+        $('#ControlTab').css({
+            "position":"inherit",
+            "margin":"0 auto",
+            "width":"480px"
+        })
+    }
+});
